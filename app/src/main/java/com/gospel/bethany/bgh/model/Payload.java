e31ebe2f1489b604e6e3a8921277373219d62327
Samuvel P <samuvelpandian@gmail.com>
2018-10-01 10:03:51 +0530

    audio payload

---
+++ app/src/main/java/com/gospel/bethany/bgh/model/Payload.java
@@ -0,0 +1,16 @@
+package com.gospel.bethany.bgh.model;
+
+public class Payload {
+    public Payload() {
+    }
+
+    public String audioUrl = "";
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+}
