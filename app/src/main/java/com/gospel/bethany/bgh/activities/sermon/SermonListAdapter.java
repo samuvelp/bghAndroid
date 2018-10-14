c4dab9b847531b73283dfbcef58598e94ecb08cd
Samuvel P <samuvelpandian@gmail.com>
2018-10-14 19:19:23 +0530

    sermon list adapter row layout modified

--- app/src/main/java/com/gospel/bethany/bgh/activities/sermon/SermonListAdapter.java
+++ app/src/main/java/com/gospel/bethany/bgh/activities/sermon/SermonListAdapter.java
@@ -7,13 +7,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.gospel.bethany.bgh.R;
+import com.gospel.bethany.bgh.customViews.SpinnerImageView;
 import com.gospel.bethany.bgh.model.Sermon;
-import com.ohoussein.playpause.PlayPauseView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class SermonListAdapter extends RecyclerView.Adapter<SermonListAdapter.MyViewHolder> {
@@ -25,7 +27,7 @@ public class SermonListAdapter extends RecyclerView.Adapter<SermonListAdapter.My
         this.mListener = (OnPlayPauseButtonClickListener) context;
     }
 
-    public void setSermonList(ArrayList<Sermon> list) {
+    void setSermonList(ArrayList<Sermon> list) {
         mSermonList = list;
     }
 
@@ -37,17 +39,16 @@ public class SermonListAdapter extends RecyclerView.Adapter<SermonListAdapter.My
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
         holder.mSermonTitle.setText(mSermonList.get(position).getTitle());
         holder.mSermonAuthor.setText(mSermonList.get(position).getAuthor());
         holder.mSermonLength.setText(getDuration(mSermonList.get(position).getPayload().getDuration()));
     }
 
     private String getDuration(int length) {
-        String hms = String.format("%02d:%02d:%02d", TimeUnit.SECONDS.toHours(length),
+        return String.format(Locale.getDefault(), "%02d:%02d:%02d", TimeUnit.SECONDS.toHours(length),
                 TimeUnit.SECONDS.toMinutes(length) % TimeUnit.HOURS.toMinutes(1),
                 TimeUnit.SECONDS.toSeconds(length) % TimeUnit.MINUTES.toSeconds(1));
-        return hms;
     }
 
     @Override
@@ -57,24 +58,27 @@ public class SermonListAdapter extends RecyclerView.Adapter<SermonListAdapter.My
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         TextView mSermonTitle, mSermonAuthor, mSermonLength;
-        PlayPauseView mPlayPauseView;
+        LinearLayout mRowLayout;
+        SpinnerImageView mSpinnerImageView;
 
         MyViewHolder(View itemView) {
             super(itemView);
+            mRowLayout = itemView.findViewById(R.id.row_view_linear_layout);
             mSermonTitle = itemView.findViewById(R.id.row_sermon_title_tv);
             mSermonAuthor = itemView.findViewById(R.id.row_sermon_author_tv);
             mSermonLength = itemView.findViewById(R.id.row_sermon_length);
-            mPlayPauseView = itemView.findViewById(R.id.row_play_pause_view);
-            mPlayPauseView.setOnClickListener(new View.OnClickListener() {
+            mSpinnerImageView = itemView.findViewById(R.id.spinning_image_view);
+            mRowLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    mListener.onPlayButtonClicked(mPlayPauseView, getAdapterPosition());
+                    mListener.onPlayButtonClicked(getAdapterPosition());
                 }
             });
         }
     }
 
+
     interface OnPlayPauseButtonClickListener {
-        void onPlayButtonClicked(PlayPauseView playPauseView, int position);
+        void onPlayButtonClicked(int position);
     }
 }
