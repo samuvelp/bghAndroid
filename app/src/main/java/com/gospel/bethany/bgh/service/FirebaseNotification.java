8d752dde6591b3eca093f05c11ae1ef024be7fe6
Samuvel P <samuvelpandian@gmail.com>
2018-12-25 11:46:28 +0530

    handled firebase notification for sermon

--- app/src/main/java/com/gospel/bethany/bgh/service/FirebaseNotification.java
+++ app/src/main/java/com/gospel/bethany/bgh/service/FirebaseNotification.java
@@ -1,11 +1,11 @@
 package com.gospel.bethany.bgh.service;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -14,6 +14,7 @@ import com.google.firebase.messaging.RemoteMessage;
 import com.gospel.bethany.bgh.R;
 import com.gospel.bethany.bgh.activities.eventcalendar.EventCalendarActivity;
 import com.gospel.bethany.bgh.activities.main.MainActivity;
+import com.gospel.bethany.bgh.activities.sermon.SermonActivity;
 import com.gospel.bethany.bgh.activities.tap.TapActivity;
 
 public class FirebaseNotification extends FirebaseMessagingService {
@@ -29,39 +30,47 @@ public class FirebaseNotification extends FirebaseMessagingService {
             notificationTitle = remoteMessage.getData().get("title");
             notificationBody = remoteMessage.getData().get("body");
             notificationType = remoteMessage.getData().get("type");
+            sendNotification(notificationTitle, notificationBody, notificationType);
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-        sendNotification(notificationTitle, notificationBody, notificationType);
     }
 
 
     private void sendNotification(String notificationTitle, String notificationBody, String notificationType) {
         Intent intent = null;
+        String CHANNEL_ID = "01";
         if (notificationType.equals("tap")) {
             intent = new Intent(this, TapActivity.class);
         } else if (notificationType.equals("calendar")) {
             intent = new Intent(this, EventCalendarActivity.class);
-        } else{
+        } else if (notificationType.equals("sermon")) {
+            intent = new Intent(this,SermonActivity.class);
+        } else {
             intent = new Intent(this, MainActivity.class);
         }
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setAutoCancel(true)   //Automatically delete the notification
-                .setSmallIcon(R.mipmap.ic_notify) //Notification icon
-                .setContentIntent(pendingIntent)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_notify)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationBody)
-                .setSound(defaultSoundUri);
-
-
+                .setContentIntent(pendingIntent)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(notificationBody))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
 
+        {
+            CharSequence name = notificationTitle;
+            String description = notificationBody;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("01", name, importance);
+            channel.setDescription(description);
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
         notificationManager.notify(0, notificationBuilder.build());
     }
 
